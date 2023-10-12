8b0b90fec164e0862567400d9150a64051307d29
Charlie Chan <cchan_03@yahoo.ca>
2023-10-11 20:15:24 -0400

    updated video path using public url

--- src/components/PlayBackground.js
+++ src/components/PlayBackground.js
@@ -6,7 +6,7 @@ const PlayBackground = () => {
   return (
     <ReactPlayer
       className="actual-video"
-      url="videos/background.mp4"
+      url={`${process.env.PUBLIC_URL}/videos/Background.mp4`}
       playing
       loop={true}
       muted
