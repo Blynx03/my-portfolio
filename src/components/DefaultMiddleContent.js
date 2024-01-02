ea631b0e24163f539ad700f399392df5062ceaff
Charlie Chan <cchan_03@yahoo.ca>
2024-01-02 17:43:59 -0500

    aligned some elements for mobile viewing

--- src/components/DefaultMiddleContent.js
+++ src/components/DefaultMiddleContent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect, useState, useContext } from "react";
+import { useContext } from "react";
 import "./defaultMiddleContent.css";
 import userContext from "./userContext";
 
